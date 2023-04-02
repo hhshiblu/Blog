afe15de2c042227f4580473f80fc92bb96f2b7c4
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-04-02 18:09:57 +0000

    M Backend/Controlar/Controlar.js  M Backend/src/app.js  M client/src/Redux/AuthAction_creator.jsx ?? .gitignore ?? Backend/yarn.lock ?? client/yarn.lock ?? yarn.lock

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,23 +1,18 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-const cookieParser = require("cookie-parser");
-const router = require("../Route/route");
-const PORT = process.env.PORT;
-const cors = require("cors");
-app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
-app.use(cookieParser());
-require("../DB/index");
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' })
+const cookieParser = require('cookie-parser')
+const router = require('../Route/route')
+const PORT = process.env.PORT
+const cors = require('cors')
+app.use(cors({ origin: /.*/, credentials: true }))
+app.use(express.json())
+app.use(cookieParser())
 
-app.use("/api/v1", router);
+require('../DB/index')
 
+app.use('/api/v1', router)
 app.listen(PORT, () => {
-  console.log(`app is listen http://localhost:${PORT}`);
-});
+  console.log(`app is listen http://localhost:${PORT}`)
+})
--- client/src/Redux/AuthAction_creator.jsx
+++ client/src/Redux/AuthAction_creator.jsx
@@ -1,5 +1,8 @@
-import axios from "axios";
-import * as ActionType from "./ActionType";
+import axios from 'axios'
+import * as ActionType from './ActionType'
+axios.defaults.withCredentials = true
+axios.defaults.baseURL =
+  location.protocol === 'http:' ? 'http://localhost:5000/api/v1' : 'server:url'
 
 export const authSuccess = (user) => {
   return {
@@ -7,16 +10,16 @@ export const authSuccess = (user) => {
     payload: {
       user: user,
     },
-  };
-};
+  }
+}
 export const failedLogin = (error) => {
   return {
     type: ActionType.AUTH_FAILED,
     payload: {
       error,
     },
-  };
-};
+  }
+}
 
 export const signUpSuccess = (massage) => {
   return {
@@ -24,31 +27,31 @@ export const signUpSuccess = (massage) => {
     payload: {
       massage,
     },
-  };
-};
+  }
+}
 export const signUpFailed = (error) => {
   return {
     type: ActionType.AUTH_SIUP_FAILED,
     payload: {
       error,
     },
-  };
-};
+  }
+}
 
-export const signUp =(firstName, email, phone, password, cpassword) => async (dispatch) => {
+export const signUp =
+  (firstName, email, phone, password, cpassword) => async (dispatch) => {
     let signUpData = {
       firstName,
       email,
       phone,
       password,
       cpassword,
-    };
+    }
 
-
-    const SIGNUP_URL = "http://localhost:5000/api/v1/signUp";
+    const SIGNUP_URL = '/signUp'
     try {
-      const response= await axios.post(SIGNUP_URL,signUpData)
-      if(response.status===200){
+      const response = await axios.post(SIGNUP_URL, signUpData)
+      if (response.status === 200) {
         dispatch(signUpSuccess(response.data.massage))
       }
     } catch (error) {
@@ -56,53 +59,45 @@ export const signUp =(firstName, email, phone, password, cpassword) => async (di
         switch (error.response.status) {
           case 403:
             dispatch(signUpFailed(error.response.data.error))
-            break;
+            break
           case 402:
             dispatch(signUpFailed(error.response.data.error))
-            break;
+            break
           case 401:
             dispatch(signUpFailed(error.response.data.error))
-            break;
+            break
           default:
-            dispatch(signUpFailed("Unknown error"))
+            dispatch(signUpFailed('Unknown error'))
         }
       } else {
-        dispatch(signUpFailed("Network Error"));
+        dispatch(signUpFailed('Network Error'))
       }
     }
-      
-  };
+  }
 
-
-
-export const signinData = (email, password) =>async (dispatch) => {
+export const signinData = (email, password) => async (dispatch) => {
   let auth = {
     email: email,
     password: password,
-  };
-  const LOGIN_URL = "http://localhost:5000/api/v1/signIn";
+  }
+  const LOGIN_URL = '/signIn'
 
-   
-
-    try {
-      const response = await axios.post(LOGIN_URL, auth);
-      if (response.status === 200) {
-        dispatch(authSuccess(response.data.user));
-        
-      }
-    
-    } catch (error) {
-      if (error.response) {
-        if (error.response.status === 401) {
-          dispatch(failedLogin(error.response.data.error));
-        } else if (error.response.status === 402) {
-          dispatch(failedLogin(error.response.data.error));
-        } else if (error.response.status === 403) {
-          dispatch(failedLogin(error.response.data.error));
-        }
-      } else {
-        dispatch(failedLogin("Network Error"));
-      }
+  try {
+    const response = await axios.post(LOGIN_URL, auth)
+    if (response.status === 200) {
+      dispatch(authSuccess(response.data.user))
     }
-   
-};
+  } catch (error) {
+    if (error.response) {
+      if (error.response.status === 401) {
+        dispatch(failedLogin(error.response.data.error))
+      } else if (error.response.status === 402) {
+        dispatch(failedLogin(error.response.data.error))
+      } else if (error.response.status === 403) {
+        dispatch(failedLogin(error.response.data.error))
+      }
+    } else {
+      dispatch(failedLogin('Network Error'))
+    }
+  }
+}
